a74426974d32a8f88a0ffcc6f3e98f5b672cbc91
liumu1006 <34349344+liumu1006@users.noreply.github.com>
2017-12-08 02:53:50 +0800

    Add files via upload

---
+++ page/talent/coolsite.config.js
@@ -0,0 +1,34 @@
+
+var actions = [];
+
+var animations = [];
+
+var timelines = [
+  {
+    "iType": 0, 
+    "isNew": true, 
+    "animations": [], 
+    "element_id": "body_61b44adc175d0e5c", 
+    "data": {
+      "type": 0, 
+      "t": {
+        "rv": 0, 
+        "rp": 0, 
+        "wa": 0, 
+        "de": 0, 
+        "st": 1, 
+        "du": 1, 
+        "es": 0
+      }, 
+      "d": {}
+    }, 
+    "id": "M_a7eba054195d61c9"
+  }
+];
+
+getApp().coolsite360.DATA[__wxRoute] = {
+   animations:animations,
+   actions:actions,
+   timelines:timelines
+};
+
